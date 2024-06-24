b30e2ca4324ff50093cf6f65f3861546ac5847bc
rcellas <rocio.cejudo.soronellas@gmail.com>
2024-06-24 23:26:16 +0200

    refactor test and controller by tagController

--- StackF5/Controllers/tagController.cs
+++ StackF5/Controllers/tagController.cs
@@ -1,11 +1,28 @@
-using Microsoft.AspNetCore.Http;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using StackF5.DTOs.Tag;
+using StackF5.Repository.Tag;
 
-namespace StackF5.Controllers
-{
-    [Route("api/[controller]")]
+namespace StackF5.Controllers;
+
     [ApiController]
+    [Route("[controller]")]
     public class tagController : ControllerBase
     {
+        private readonly ITagRepository _tagRepository;
+        private readonly IMapper _mapper;
+
+        public tagController(ITagRepository tagRepository, IMapper mapper)
+        {
+            _tagRepository = tagRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<TagDto>>> GetAllTags()
+        {
+            var tags = await _tagRepository.GetAllTags();
+            var tagDtos = _mapper.Map<List<TagDto>>(tags);
+            return Ok(tagDtos);
+        }
     }
-}
--- StackF5Test/Controllers/TagController.cs
+++ StackF5Test/Controllers/TagController.cs
@@ -1,37 +1,46 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using StackF5.Controllers;
 using StackF5.DTOs.Tag;
+using StackF5.Entity;
 using StackF5.Repository.Tag;
+using Xunit;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace StackF5Test.Controllers;
+    public class TagControllerTest
+    {
+        private readonly Mock<ITagRepository> _mockRepository;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly tagController _controller;
 
-public class TagController
-{
-    private readonly Mock<ITagRepository> _mockRepository;
-    private readonly Mock<IMapper> _mockMapper;
-    private readonly TagController _controller;
-    public TagController()
-    {
-        _mockMapper= new Mock<IMapper>();
-        _mockRepository= new Mock<ITagRepository>();
-        _controller= new TagController(null,_mockRepository.Object, _mockMapper.Object);
+        public TagControllerTest()
+        {
+         
+            _mockRepository = new Mock<ITagRepository>();
+            _mockMapper = new Mock<IMapper>();
+            
+            _controller = new tagController(_mockRepository.Object, _mockMapper.Object);
+        }
+
+        [Fact]
+        public async Task TagController_GetAllTags_ShouldReturnOk_WhenTagsExist()
+        {
+            
+            var tags = new List<Tag> { new Tag { Id = 1, Name = "Test Tag" } };
+            var expectedTagDtos = new List<TagDto> { new TagDto { Id = 1, Name = "Test Tag" } };
+            
+            _mockRepository.Setup(repo => repo.GetAllTags()).ReturnsAsync(tags);
+            _mockMapper.Setup(m => m.Map<List<TagDto>>(It.IsAny<List<Tag>>())).Returns(expectedTagDtos);
+            
+            var result = await _controller.GetAllTags();
+            
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            Assert.NotNull(okResult);
+
+            var actualTagDtos = Assert.IsType<List<TagDto>>(okResult.Value);
+            Assert.Equal(expectedTagDtos, actualTagDtos);
+        }
     }
-    
-    [Fact]
-    public async Task TagController_GetAllTags_ShouldReturnOk_WhenTagsExist()
-    {
-        // Arrange
-        var expectedTags = new List<TagDto>() { new TagDto { Id = 1, Name = "Test Tag" } };
-        _mockRepository.Setup(repo => repo.GetAllTags()).ReturnsAsync(expectedTags);
-        
-        // Act
-        var result = await _controller.GetAllTags();
-        
-        // Assert
-        Assert.IsType<OkObjectResult>(result.Result); // Verify response type
-        var okResult = result.Result as OkObjectResult; // Cast to OkObjectResult
-        Assert.NotNull(okResult); // Check if OkObjectResult is not null
-        Assert.Equal(expectedTags, okResult.Value); // Verify tag list in response
-    }
-}
